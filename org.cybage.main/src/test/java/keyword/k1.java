b9043740de21186cc5aa4ca7e27522484a10c488
HP <HP@DESKTOP-5EFFPEF>
2022-11-24 21:57:37 +0530

    Ashutosh Patil

--- org.cybage.main/src/test/java/keyword/k1.java
+++ org.cybage.main/src/test/java/keyword/k1.java
@@ -1,5 +1,23 @@
 package keyword;
 
-public class k1 {
+import java.io.IOException;
 
-}
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class k1 {
+	public static void main(String[] args) throws InterruptedException, IOException {
+	 	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	 		WebDriver driver=new ChromeDriver();
+	 	    driver.get("https://www.flipkart.com/");
+	 		Thread.sleep(2000);
+	 		
+	 		driver.manage().window().maximize();
+	 		
+	 		Actions a=new Actions(driver);
+	 		a.sendKeys(Keys.ARROW_DOWN);
+	 		
+
+}}
